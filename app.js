792868e3360a5d47a844f6aafb22c7d42e24108e
sidharsha <siddharth.landers@gmail.com>
2020-09-07 22:48:50 -0400

    Created a sample node app ruuning on a server with port 3000

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
 console.log("Hello World");
+
+app.get('',(req,res)=>{
+    res.send("<h1>This code runs on the server</h1>");
+});
+
+app.listen(port,()=>{
+    console.log("The server is running on port "+port);
+});
